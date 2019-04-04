21f3b32655c1be41139c9f347fb657ec0b68cf5f
Ivan Mironovich <imironovich@gridgain.com>
2019-04-04 19:24:18 +0300

    without auth, with mysql

--- src/main/java/ru/afpf/kalugahillspara/AnalizWeatherForecast.java
+++ src/main/java/ru/afpf/kalugahillspara/AnalizWeatherForecast.java
@@ -31,19 +31,21 @@ public class AnalizWeatherForecast extends TimerTask {
             try (Connection connection = DriverManager.getConnection(sqlurl, sqluser, sqlpasswd)) {
                 LocalDate daten = LocalDate.now();
 
-                for (int i = 0; i < 9; i++) {
+                for (int i = 1; i < 9; i++) {
                     LocalDate thisDay = daten.plusDays(i);
+                   // System.out.println(thisDay);
                     for (int j = 0; j < 8; j++) {
 
                         int hours3 = (j * 3);
                         if (8 < hours3 & hours3 < 19) {
+                           // System.out.println(hours3);
                             Statement stmt = connection.createStatement();
                             ResultSet rersult = stmt.executeQuery(
                                     "SELECT windSpeedInt, windDirInt, windBoostInt, rainInt FROM windGuru WHERE date = '" + thisDay + "' AND hour = '" + hours3 + "'");
 
                             if (rersult.next()) {
-                                if (rersult.getInt("rainInt") == 0) {
-                                    if ((rersult.getInt("windSpeedInt") > 4 && (rersult.getInt("windBoostInt") < 9)) && (200 > rersult.getInt("windDirInt")) && (rersult.getInt("windDirInt") > 160)) {
+                                //if (rersult.getInt("rainInt") <= 2) {
+                                    if ((rersult.getInt("rainInt") <= 2) && (rersult.getInt("windSpeedInt") >= 4 && (rersult.getInt("windBoostInt") <= 9)) && (200 > rersult.getInt("windDirInt")) && (rersult.getInt("windDirInt") > 160)) {
                                         System.out.println(
                                                 thisDay + " " + hours3 + " " + rersult.getInt("rainInt") + " " + rersult.getInt("windSpeedInt") + " " + rersult.getInt("windSpeedInt") + " " + rersult.getInt("windBoostInt") + " " + rersult.getInt("windDirInt"));
                                         PreparedStatement preparedStatement = connection.prepareStatement(sqlFlying);
@@ -52,8 +54,9 @@ public class AnalizWeatherForecast extends TimerTask {
                                         preparedStatement.setInt(3, 1); //ЖЕлохово
                                         preparedStatement.addBatch();
                                         preparedStatement.executeBatch();
+                                       // System.out.println("Жел");
                                     }
-                                    if ((rersult.getInt("windSpeedInt") > 5 && (rersult.getInt("windBoostInt") < 11)) && (290 > rersult.getInt("windDirInt")) && (rersult.getInt("windDirInt") > 250)) {
+                                    if ((rersult.getInt("rainInt") <= 2) && (rersult.getInt("windSpeedInt") >= 5 && (rersult.getInt("windBoostInt") <= 11)) && (290 > rersult.getInt("windDirInt")) && (rersult.getInt("windDirInt") > 250)) {
                                         System.out.println(
                                                 thisDay + " " + hours3 + " " + rersult.getInt("rainInt") + " " + rersult.getInt("windSpeedInt") + " " + rersult.getInt("windSpeedInt") + " " + rersult.getInt("windBoostInt") + " " + rersult.getInt("windDirInt"));
                                         PreparedStatement preparedStatement = connection.prepareStatement(sqlFlying);
@@ -62,15 +65,18 @@ public class AnalizWeatherForecast extends TimerTask {
                                         preparedStatement.setInt(3, 2); //Вороново
                                         preparedStatement.addBatch();
                                         preparedStatement.executeBatch();
+                                       // System.out.println("Ворон");
                                     }
-                                } else {
-                                    PreparedStatement preparedStatement = connection.prepareStatement(sqlFlying);
-                                    preparedStatement.setObject(1, thisDay);
-                                    preparedStatement.setInt(2, hours3);
-                                    preparedStatement.setInt(3, 0);
-                                    preparedStatement.addBatch();
-                                    preparedStatement.executeBatch();
-                                }
+                            //    }
+                                     else {
+                                        PreparedStatement preparedStatement = connection.prepareStatement(sqlFlying);
+                                        preparedStatement.setObject(1, thisDay);
+                                        preparedStatement.setInt(2, hours3);
+                                        preparedStatement.setInt(3, 0);
+                                        preparedStatement.addBatch();
+                                        preparedStatement.executeBatch();
+                                       // System.out.println("нигде");
+                                    }
                             }
                         }
                     }
